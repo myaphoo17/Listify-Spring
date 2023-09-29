4c937805562a28b27036fb679dc540c97b0b4c8a
Mya Phoo Thwe <75157807+myaphoo17@users.noreply.github.com>
2023-09-30 00:31:38 +0630

    Add files via upload

---
+++ Listify-Backend/src/main/java/com/listify/demo/ListifyBackendApplication.java
@@ -0,0 +1,12 @@
+package com.listify.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListifyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListifyBackendApplication.class, args);
+    }
+}
